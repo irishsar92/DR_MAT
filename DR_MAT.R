9e46235d20ddf66736e8c562b665ff31074fa3b2
Sara Irish <irishsar92@gmail.com>
2024-10-25 13:17:54 +0100

    DRO Reproduction

--- DR_MAT.R
+++ DR_MAT.R
@@ -81,8 +81,7 @@ DRO_LS3$Treatment <- as.factor(DRO_LS3$Treatment)
 DRO_LS3$Treatment <- relevel(DRO_LS3$Treatment, ref = 'F')
 DRO_LS2$Treatment <- relevel(DRO_LS2$Treatment, ref = 'F')
 
-
-###EV or DAF treatment only
+#Function for creating forest plot
 meforest <- function(cox, F){  #Eds version of forest plot
   require(AICcmodavg)
   require(ggplot2)
@@ -137,3 +136,115 @@ DR_all_plot <- ggarrange(LS_plot$plot, LS_plot_matcen$plot, forest, forest_matce
 DR_all_plot
 ggsave('DR_all_plot.tif', height = 8, width = 12)
 
+#test cox models for fit
+cox.zph(cox)#looks good - use cox model results
+cox.zph(cox_matcen)#looks good
+
+
+
+
+
+
+
+
+
+# DRO Reproduction --------------------------------------------------------
+
+
+DR_rep <- read.csv('DR_Lab_repro_2.csv')
+
+rep_long <- DR_rep %>% 
+  pivot_longer(
+    cols = `D1`:`D8`, 
+    names_to = "Day",
+    values_to = "value"
+  )
+
+rep_long$Treatment <- factor(rep_long$Treatment, levels = c('F','FO','DR','DRO'))
+
+rep_long$Treatment <- revalue(rep_long$Treatment, c('F' = 'F', 'FO' = 'F+O', 'DR' = 'DR', 'DRO' = 'DR+O'))
+
+## Age-specific reproduction plot
+rep <-ggplot(data=rep_long, aes(x=factor(Day), y=value, group=Treatment, color=Treatment))+
+  geom_jitter(alpha = 0.2, position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.5))+
+  stat_summary(fun.data="mean_cl_boot", geom="errorbar", size = 1, width=0.0, position = position_dodge(0.5)) +
+  stat_summary(fun.data="mean_cl_boot", geom="point", size = 3, position = position_dodge(0.5)) +
+  stat_summary(fun.data="mean_cl_boot", geom="line",  size=1, position = position_dodge(0.5)) +
+  theme_classic()+
+  labs(y="Offspring number", x="", size=20)+
+  labs(col="")+
+  theme(axis.title.y = element_text(size=12))+
+  theme(axis.title.x = element_text(size=12))+
+  theme(legend.key.width = unit(0.5,"cm"))+
+  coord_cartesian(ylim = c(0,170))+
+  theme(legend.position = c(0.8,0.9))+
+  scale_colour_manual(values = palette)
+
+rep
+
+#Sum number of offspring over 5 days for individuals
+totalrep<-na.omit(as.data.frame.table(tapply(rep_long$value,list(rep_long$Treatment, rep_long$ID),sum)))
+
+
+#rename columns
+names(totalrep)<-c("Treatment", "Replicate", "Totrep")
+
+totrep_dab <-
+  totalrep %>%
+  load(x =Treatment, y=Totrep,
+       idx = c('F','F+O','DR','DR+O'))
+
+totrep_p <- mean_diff(totrep_dab)
+
+tot_rep_plot <- dabest_plot(totrep_p, FALSE, raw_marker_spread = 1, swarm_label = 'LRS',custom_palette = 'd3')
+
+tot_rep_plot
+
+#### Lambda
+
+#spread data back out
+rep_wide <- spread(rep_long, Day, value)
+
+rep_wide <- na.omit(rep_wide)
+
+#calculate Lambda for each individual
+L <- matrix(nrow = nrow(rep_wide), ncol = 4)
+
+for (i in 1:nrow(rep_wide)){
+  Les <- matrix(0, ncol = 10, nrow = 10) #ncol and nrow should = no. days repro +2
+  diag(Les[-1,]) <- rep(1, 9) # add the 1s for survival probability diagonally
+  Fert <- c(0,0, as.numeric(as.vector(rep_wide[i,][4:11]))) #the columns in data that has the reproductive counts
+  Fert[is.na(Fert)] <- 0 #makes all NAs into 0s
+  Les[1,] <- c(Fert)
+  class(Les) <- "leslie.matrix"
+  Lambda <- popbio::eigen.analysis(Les)$lambda1
+  L[i, 1:4] <- c(paste0(rep_wide$Block[i]),paste0(rep_wide$ID[i]), paste0(rep_wide$Treatment[i]), Lambda)
+  
+}
+
+#rename columns
+colnames(L)<-c("Block", "ID", "Treatment","Lambda")
+
+#make L a data frame
+Data<-as.data.frame(L)
+
+#make sure it's numeric
+Data$Lambda<-as.numeric(as.character(Data$Lambda))
+
+str(Data)
+
+#create dabestr plot
+Lambda_rep <-
+  Data %>%
+  load(Treatment, Lambda,
+       idx = list(c("F", "F+O", "DR", "DR+O")))
+
+Lambda_rep_dab <- mean_diff(Lambda_rep)
+
+lam_plot <- dabest_plot(Lambda_rep_dab, FALSE, swarm_label = 'Lambda', raw_marker_spread = 1, custom_palette = 'd3')
+
+lam_plot
+
+rep/(tot_rep_plot+lam_plot)
+
+ggsave('DRO_reproduction.tif', height = 8, width = 10)
